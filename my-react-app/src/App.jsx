760755ffb95a05d7c74e94d7936f89e314c28d93
satyampal <em.satyam@gmail.com>
2025-12-23 05:37:17 +0400

    React-Project #S000N

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./Header.jsx"
+import Footer from "./Footer.jsx"
+import Food from "./Food.jsx"
+
+function App() {
+    return(
+       <>  {/* Fragment opening and closing empty tags are */}
+        <Header> </Header>
+        <Food></Food>
+        <Footer></Footer>
+      </>
+    )
+}
+
+export default App
