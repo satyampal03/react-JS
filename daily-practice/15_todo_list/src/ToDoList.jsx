9dc3a0d3ce00fd0e59692c341c17be70f9fff811
satyampal <em.satyam@gmail.com>
2025-12-26 13:34:55 +0400

    React JS ------First Project -- TO-DO-LIST�

---
+++ daily-practice/15_todo_list/src/ToDoList.jsx
@@ -0,0 +1,127 @@
+import React, {useState} from 'react';
+
+function ToDoList(){
+    const [tasks, setTasks] = useState(['xTask','xTask']);
+    const [newTask, setNewTask] = useState('');
+
+    const [newCompletedTask, setNewCompletedTask] = useState([]);
+
+
+
+    function updateNewTaskValue(event){
+       setNewTask(event.target.value);
+    }
+
+    function addTask(){  
+
+        if(newTask.trim() !== ''){
+            setTasks(t => ([...t, newTask]));
+            setNewTask('')
+        }
+       
+    }
+
+    function deleteTask(index){
+        const updateTask = tasks.filter((_,i)=>{
+            return i !== index;
+        })
+
+        setTasks(updateTask);
+    }
+
+    function moveTaskUp(index){
+        if(index > 0){
+            const updateTaskUp = [...tasks];
+            [updateTaskUp[index], updateTaskUp[index-1]] = [updateTaskUp[index-1], updateTaskUp[index]]
+
+            setTasks(updateTaskUp);
+
+        }
+
+    }
+
+
+    function moveTaskDown(index){
+            if(index < tasks.length-1){
+                const updateTaskDown = [...tasks];
+                [updateTaskDown[index], updateTaskDown[index+1]] = [updateTaskDown[index+1], updateTaskDown[index]]
+
+                setTasks(updateTaskDown);
+            }
+    }
+
+
+// Completed Task List
+
+        function completedTask(index){
+
+        const updateCompletedList = tasks.filter((_,i)=>{
+            return i == index;
+        })
+        setNewCompletedTask(x => ([...x, updateCompletedList]));
+
+        const updatePendingList = tasks.filter((_,i)=>{
+            return i !== index;
+        })
+        setTasks(updatePendingList);
+
+        console.log(updateCompletedList ,updatePendingList);
+
+    }
+
+
+    function deleteCompletedTask(index){
+        const completedTaskUpdater = newCompletedTask.filter((_,i)=>{
+            return i !== index;
+        })
+
+        setNewCompletedTask(completedTaskUpdater)
+
+
+    }
+
+    return (
+        <>
+        <div className='to-do-list' >
+            <h1 className='project-name'>TO DO LIST</h1>
+            <div className='newTask-add'>
+                <input type="text" placeholder='Enter A Task' value={newTask} onChange={updateNewTaskValue}/>
+                <button className='add-task' onClick={addTask}>Add</button>
+                
+            </div>
+        <div className='pendingTask-Container'>
+            <ol>
+            <h2 className='pending'> Pending Taks</h2>
+            {tasks.map((task, index)=>{
+                return <li key={index}><span className='task'> {task}</span> 
+                   <div className='task-crud'>
+                    <button className='deleteTask' onClick={() => deleteTask(index)}>Delete</button>
+                    <button className='completedTask' onClick={() => completedTask(index)}>Completed</button>
+                    <button className='moveTask' onClick={() => moveTaskUp(index)}>⬆️</button>
+                    <button className='moveTask' onClick={() => moveTaskDown(index)}>⬇️</button>
+                    
+                    </div> 
+                </li>
+            })}
+           </ol>
+        </div>
+
+
+     <div className='completedTask-Container'>
+                <ol>
+            <h2 className='completed'> Completed Taks</h2>
+            {newCompletedTask.map((task, index)=>{
+                return <li key={index}><span className='task'> {task}</span> 
+                   <div className='task-crud'>
+                    <button className='deleteTask' onClick={() => deleteCompletedTask(index)}>Delete</button>
+                    </div> 
+                </li>
+            })}
+           </ol>
+           </div>
+        </div>
+        </>
+    )
+}
+
+export default ToDoList;
