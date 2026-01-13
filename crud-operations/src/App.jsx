bf7eac33eaa8cef5981b2c9be9735cab8aa52b18
satyampal <em.satyam@gmail.com>
2026-01-13 11:26:57 +0400

    Minor File Changes

--- crud-operations/src/App.jsx
+++ crud-operations/src/App.jsx
@@ -1,35 +1,9 @@
 import React, { useEffect, useState } from "react";
 import {EmployData} from "./EmployData";
-import supabase from "./config/superbaseClient";
 
 function App (){
 
-  // const [fetchError, setFetchError] = useState(null);
-  // const [smoothies, setSmoothies] = useState(null);
-
-
-  // useEffect(()=> {
-  //   const fetchSmoothies = async() =>{
-  //     const {data, error} = await supabase
-  //       .from('smoothies')
-  //       .select()
-
-  //       if(error){
-  //         setFetchError('Could not fetch the smoothies');
-  //         setSmoothies(null);
-  //         console.log(error);
-  //       }
-  //       if(data){
-  //         setSmoothies(data);
-  //         setFetchError(null);
-
-  //       }
-
-  //   }
-  //   fetchSmoothies();
-    
-  // },[])
-
+ 
 
   const [data, setData] = useState([]);
 
