0325f956fef6a9e7e4c20c1db146b372977b60f7
satyampal <em.satyam@gmail.com>
2026-02-08 17:33:33 +0400

    Single-Selection Accordion --react js

---
+++ React25Projects/Accordion_component/src/components/data.jsx
@@ -0,0 +1,29 @@
+const data = [
+  {
+    id: '1',
+    question: "What are accordion components?",
+    answer:
+      "Accordion components are user interface elements used for organizing content."
+  },
+  {
+    id: '2',
+    question: "What are they used for?",
+    answer:
+      "They are commonly employed in various contexts, including FAQs."
+  },
+  {
+    id: '3',
+    question: "Accordion as a musical instrument",
+    answer:
+      "The accordion is a musical instrument with a keyboard and bellows."
+  },
+  {
+    id: '4',
+    question: "Can I create an accordion component with a different framework?",
+    answer:
+      "Yes of course, it is very possible to create an accordion component using different frameworks."
+  }
+];
+
+
+export default data;
